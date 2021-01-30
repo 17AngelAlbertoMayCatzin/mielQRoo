9e500de6571af93bf3d06731f3d9ba635729934d
17AngelAlbertoMayCatzin <Cristtiangel07>
2021-01-30 01:38:58 -0500

    Creando archivo Js de comerciantes, además del Json de los comerciantes

---
+++ js/comerciantes.js
@@ -0,0 +1,46 @@
+//variables
+var comerciantes;
+var cardContainer;
+var createTaskCard;
+var card;
+var cardImg;
+var cardBody
+var descripcionL;
+var title;
+var boton;
+
+//Creando json de comerciantes
+comerciantes = [{
+        "id": "1",
+        "nombreComerciante": "Angel Alberto May Catzin",
+        "descripcion": "Vendo miel , tengo 23 kilos de miel pura, contactar para más información",
+        "img": "img/1.jpeg",
+        "municipio": "Felipe Carrillo Puerto",
+        "localidad": "Andres Quintana Roo",
+        "telefono": "9831125674",
+        "correo": "a.a.m.cat@gmail.com",
+        "precioVenta": "$37",
+    },
+    {
+         "id": "2",
+        "nombreComerciante": "Jose Alejandro Lopez",
+        "descripcion": "Soy apicultor de la comunidad de Limones, tengo disponible 50 kilos de miel a excelente precio. Contactame para más información",
+        "img": "img/2.jpg",
+        "municipio": "Bacalar",
+        "localidad": "Limones",
+        "telefono": "9831127869",
+        "correo": "j.a.l@gmail.com",
+        "precioVenta": "$40",
+    },
+    {
+         "id": "3",
+        "nombreComerciante": "Felipe de Jesús Dominguez Hernandez",
+        "descripcion": "Soy apicultor de la comunidad de , tengo disponible 10 kilos de miel a excelente precio. Contactame para más información",
+        "img": "img/3.jpg",
+        "municipio": "Felipe Carrillo Puerto",
+        "localidad": "Tusik",
+        "telefono": "9831127869",
+        "correo": "f.jesus.dH@gmail.com",
+        "precioVenta": "$29",
+    }
+];
